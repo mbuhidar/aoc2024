6c6f9399eea92b2076c12dd71d9dd33557b03a76
Michael Buhidar <mbuhidar@gmail.com>
2024-12-23 18:50:52 -0500

    Add Day05, part2.py - topological sort using DFS.

--- day05/part2.py
+++ day05/part2.py
@@ -2,19 +2,70 @@ from __future__ import annotations
 
 import argparse
 import os.path
-
+from collections import defaultdict
 import pytest
-
 import support
 
 INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 
+# Class to represent a graph using adjacency list representation
+class Graph:
+    """
+    Class to represent a graph.
+    """
+
+    def __init__(self, vertices):
+        """
+        Initialize the graph with a given number of vertices.
+        """
+        self.graph = defaultdict(list)  # dictionary containing adjacency List
+        self.num_vertices = vertices  # No. of vertices
+
+    def add_edge(self, u, v):
+        """
+        Function to add an edge to the graph.
+        """
+        self.graph[u].append(v)
+
+    def topological_sort_util(self, v, visited, stack):
+        """
+        A recursive function used by topological_sort.
+        """
+        # Mark the current node as visited.
+        visited[v] = True
+
+        # Recur for all the vertices adjacent to this vertex
+        for i in self.graph[v]:
+            if visited[i] is False:
+                self.topological_sort_util(i, visited, stack)
+
+        # Push current vertex to stack which stores result
+        stack.insert(0, v)
+
+    def topological_sort(self):
+        """
+        The function to do Topological Sort. It uses recursive topological_sort_util.
+        """
+        # Mark all the vertices as not visited
+        visited = [False] * self.num_vertices
+        stack = []
+
+        # Call the recursive helper function to store Topological
+        # Sort starting from all vertices one by one
+        for i in range(self.num_vertices):
+            if visited[i] is False:
+                self.topological_sort_util(i, visited, stack)
+
+        # Return contents of stack
+        return stack
+
+
 def compute(s: str) -> int:
     # split input on blank line
-    input = s.split('\n\n')
-    rules = input[0].splitlines()
-    pages = input[1].splitlines()
+    input1 = s.split('\n\n')
+    rules = input1[0].splitlines()
+    pages = input1[1].splitlines()
 
     middle_page = 0
     bad_pages = []
@@ -39,23 +90,21 @@ def compute(s: str) -> int:
     for page in bad_pages:
         flag = False
         page = page.split(',')
+        g = Graph(len(page))
         for rule in rules:
             rule = rule.split('|')
             if (rule[0]) in page and (rule[1]) in page:
                 index_rule1 = int(page.index(rule[0]))
                 index_rule2 = int(page.index(rule[1]))
-                print()
-                print(page)
-                print(rule)
-                if index_rule1 > index_rule2:
-                    page[index_rule1], page[index_rule2] = page[index_rule2], page[index_rule1]
-                    flag = True
-                print(page)
+                g.add_edge(index_rule1, index_rule2)
+                flag = True
 
         if flag:
-            middle_page += int(page[len(page) // 2])
+            order = g.topological_sort()
+            page_ordered = [page[i] for i in order]
+            middle_page += int(page_ordered[len(page) // 2])
 
-    return middle_page  # not 10389, 5859
+    return middle_page  # 5723
 
 
 INPUT_S = '''\
